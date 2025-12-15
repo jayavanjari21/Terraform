d02279332c730d8c4428d6ea2520adf14476bc90
jayavanjari21 <jayavanjari2101@gmail.com>
2025-12-15 16:27:29 +0530

    vpc files added

---
+++ vpc/modules/var.tf
@@ -0,0 +1,15 @@
+variable "project" {}
+
+variable "vpc_cidr_block" {}
+
+variable "vpc_cidr_pub_subnet" {}
+
+variable "vpc_cidr_pri_subnet" {}
+
+variable "region" {}
+
+variable "name"{}
+
+variable "vpc_id" {}
+
+variable "public_subnets" {}
