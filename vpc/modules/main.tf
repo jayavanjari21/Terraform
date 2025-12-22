4332b5b4753c6ffe0b3a0c479322e3e4ccf48f7c
jayavanjari21 <jayavanjari2101@gmail.com>
2025-12-22 16:52:24 +0530

    main.tf files added

--- vpc/modules/main.tf
+++ vpc/modules/main.tf
@@ -1,15 +1,19 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_vpc" "my_vpc" {
     cidr_block = var.vpc_cidr_block
-    
+
     tags = {
         Name = "${var.project}-vpc"
     }
 }
 
 resource "aws_subnet" "pub_subnet" {
-  cidr_block = var.vpc_cidr_pub_subnet
   vpc_id     = aws_vpc.my_vpc.id
-  availability_zone = "ap-south-1a"
+  cidr_block = var.vpc_cidr_pub_subnet
+  availability_zone = "us-east-2a"
   map_public_ip_on_launch = true
    tags = {
         Name = "${var.project}-pub-subnet"
@@ -17,10 +21,9 @@ resource "aws_subnet" "pub_subnet" {
 }
 
 resource "aws_subnet" "pri_subnet" {
-  cidr_block = var.vpc_cidr_pri_subnet
   vpc_id     = aws_vpc.my_vpc.id
-  availability_zone = "ap-south-1a"
-  #map_public_ip_on_launch = true
+  cidr_block = var.vpc_cidr_pri_subnet
+  availability_zone = "us-east-2a"
    tags = {
         Name = "${var.project}-pri-subnet"
     }
@@ -35,11 +38,24 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_default_route_table" "my_rt" {
-  default_route_table_id = aws_vpc.my_vpc.default_route_table_id
-
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.pub_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.pri_subnet.id
+  route_table_id = aws_route_table.private_rt.id
 }
