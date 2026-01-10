1b75692c72131d0f8f784ccec71e655256c7ed59
jayavanjari21 <jayavanjari2101@gmail.com>
2026-01-10 22:28:02 +0530

    Delete vpc directory

--- vpc/vpc.tf
+++
@@ -1,9 +0,0 @@
-module "vpc" {
-  source = "./modules"
-  region = "us-east-2"
-  project                = "cdecb45"
-  vpc_cidr_block         = "10.0.0.0/16"
-  vpc_cidr_pub_subnet    = "10.0.1.0/24"
-  vpc_cidr_pri_subnet    = "10.0.2.0/24"
-}
-
